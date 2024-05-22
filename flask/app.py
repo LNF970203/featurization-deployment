6652a0e30e81db0765ca83aeedeffd84f9beda09
Lachin Fernando <77132441+LNF970203@users.noreply.github.com>
2024-05-22 02:28:36 +0000

    check tensorflow compatability

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, jsonify
+import tensorflow as tf
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, Flask!: {}".format(tf.__version__)
 
 @app.route('/save_data')
 def save_data():
